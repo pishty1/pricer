80f3db8212556eee6edf98e0133fb834d67afb68
pishty1 <pishty1@gmail.com>
2025-04-13 11:41:59 +0100

    init

---
+++ app/src/main/java/org/example/Random1.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Random1 {
+    public static double getGaussianBySummation() {
+        double x = 0;
+        for (int i = 0; i < 12; i++) {
+            x += Math.random();
+        }
+        return x - 6.0;
+    }
+
+    public static double getGaussianByBoxMuller() {
+        double x = 0;
+        double y = 0;
+        double euclidSquare = 0;
+
+        do {
+            x = 2.0 * Math.random() - 1.0;
+            y = 2.0 * Math.random() - 1.0;
+            euclidSquare = x * x + y * y;
+        } while (euclidSquare >= 1.0);
+
+        return x * Math.sqrt(-2.0 * Math.log(euclidSquare) / euclidSquare);
+        
+    }
+}
